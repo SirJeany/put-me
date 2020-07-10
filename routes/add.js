abc99326ed0b3158fdca172147960d0af7c3a546
SirJeany <jmvanriet1@gmail.com>
2020-07-11 00:17:46 +0200

    changed header and footer to start and end. changed up index layout. New route for ADD

---
+++ routes/add.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Show to add link to new property. Scraping and adding of etra info to be implemented.
+router.get('/add', function(req, res, next) {
+
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'New title' });
+  res.render('index', { title: 'Put me' });
 });
 
 module.exports = router;
