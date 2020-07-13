fcd202f27c8643042e5a5969d8c98be377a21322
SirJeany <jmvanriet1@gmail.com>
2020-07-13 20:32:08 +0200

    added mongoose and bodyparser to project as dependiencies. Working on the post route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // todo get db info of all props.
   res.render('index', { title: 'Put me' });
 });
+
+router.post('/add-property', function(req, res, next) {
+  res.send(req.body);
+})
 
 module.exports = router;
